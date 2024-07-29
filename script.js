a2df4ba5391d0ed1c7a21607732fbd2f4fac33fa
ajaykumarsaini231 <145220614+ajaykumarsaini231@users.noreply.github.com>
2024-07-29 23:02:21 +0530

    added all outpages

---
+++ script.js
@@ -0,0 +1,231 @@
+
+    window.__CONFIG__ = {
+        "APP_ENV":"production",
+        "SELLER_SERVICE_HOST":"https://sapi.mini.store",
+        "CONSUMER_SERVICE_HOST":"https://capi.mini.store",
+        "MEDIA_BASE_URL":"https://res.cloudinary.com/swiggymini/image/upload/mini-assets",
+        "CHAT_SERVICE_HOST":"https://chat.mini.store",
+        "PUBNUB_PUBLISH_KEY":"pub-c-9fb02443-4dcc-4972-8e59-e41a0d2934c4",
+        "PUBNUB_SUBSCRIBE_KEY":"sub-c-648b1de8-c565-11ec-b36c-a6fdca316470",
+        "CASHFREE_ENDPOINT":"https://api.cashfree.com/pg",
+        "JUSPAY_ENDPOINT":"https://api.juspay.in",
+        "ANALYTICS_APP_NAME":"minis-consumer-app",
+        "ANALYTICS_ENABLED":"true",
+        "ANALYTICS_EVENT_NAME":"MinisConsumerEvent",
+        "ANALYTICS_SCHEMA_VERSION":"1.0.0",
+        "ANALYTICS_URL":"https://analytics.swiggy.com",
+        "GOOGLE_API_KEY":"AIzaSyDUyd2rm-EKfPQLdX7NWxZ--sv-5xz63BM",
+        "NEW_RELIC_INSIGHTS_ENDPOINT":"https://insights-collector.newrelic.com/v1/accounts/737486/events",
+        "NEW_RELIC_API_KEY":"CPDMQSXvjxGCdL_E02P9eZ-NJcEg-uGk",
+        "IMAGEKIT_MEDIA_BASE_URL":"https://minis-media-assets.swiggy.com",
+        "IMAGEKIT_CLOUDNAME":"swiggymini"
+    };
+
+    window.SERVICE_CONFIG = {
+        "BUILD_NUMBER":"1.0.00-16944"
+    };
+
+    const arr = ['assets/asset\ 0.jpeg','assets/asset\ 10.jpeg' ,'assets/asset\ 4.jpeg','assets/asset\ 5.jpeg','assets/asset\ 6.jpeg', 'assets/asset\ 7.jpeg','assets/asset\ 8.jpeg','assets/asset\ 9.jpeg','assets/asset\ 3.jpeg','assets/asset\ 11.jpeg','assets/asset\ 12.jpeg','assets/asset\ 13.jpeg','assets/asset\ 14.jpeg','assets/asset\ 14.jpeg','assets/asset\ 15.jpeg','assets/asset\ 17.jpeg','assets/asset\ 18.jpeg','assets/asset\ 19.jpeg','assets/asset\ 20.jpeg','assets/asset\ 21.jpeg','assets/asset\ 22.jpeg','assets/asset\ 23.jpeg','assets/asset\ 24.jpeg','assets/asset\ 25.jpeg','assets/asset\ 26.jpeg','assets/asset\ 27.jpeg','assets/asset\ 28.jpeg','assets/asset\ 29.jpeg','assets/asset\ 30.jpeg','assets/asset\ 31.jpeg','assets/asset\ 32.jpeg','assets/asset\ 33.jpeg'];
+
+    const anime = [
+        'assets/Animated_3d_poster/asset\ 34.jpeg','assets/Animated_3d_poster/asset\ 35.jpeg', 'assets/Animated_3d_poster/asset\ 36.jpeg','assets/Animated_3d_poster/asset\ 37.jpeg', 'assets/Animated_3d_poster/asset\ 51.jpeg', 'assets/Animated_3d_poster/asset\ 38.jpeg','assets/Animated_3d_poster/asset\ 52.jpeg','assets/Animated_3d_poster/asset\ 39.jpeg','assets/Animated_3d_poster/asset\ 50.jpeg', 'assets/Animated_3d_poster/asset\ 40.jpeg','assets/Animated_3d_poster/asset\ 49.jpeg','assets/Animated_3d_poster/asset\ 41.jpeg', 'assets/Animated_3d_poster/asset\ 48.jpeg','assets/Animated_3d_poster/asset\ 42.jpeg','assets/Animated_3d_poster/asset\ 47.jpeg','assets/Animated_3d_poster/asset\ 43.jpeg', 'assets/Animated_3d_poster/asset\ 46.jpeg', 'assets/Animated_3d_poster/asset\ 44.jpeg','assets/Animated_3d_poster/asset\ 45.jpeg'
+    ]
+
+    const animemetalcot = [
+        'assets/anime_poster/asset\ 53.jpeg','assets/anime_poster/asset\ 67.jpeg', 'assets/anime_poster/asset\ 87.jpeg','assets/anime_poster/asset\ 101.jpeg', 'assets/anime_poster/asset\ 60.jpeg','assets/anime_poster/asset\ 73.jpeg', 'assets/anime_poster/asset\ 80.jpeg','assets/anime_poster/asset\ 100.jpeg', 'assets/anime_poster/asset\ 108.jpeg','assets/anime_poster/asset\ 54.jpeg', 'assets/anime_poster/asset\ 68.jpeg','assets/anime_poster/asset\ 88.jpeg', 'assets/anime_poster/asset\ 102.jpeg','assets/anime_poster/asset\ 61.jpeg', 'assets/anime_poster/asset\ 74.jpeg','assets/anime_poster/asset\ 81.jpeg', 'assets/anime_poster/asset\ 99.jpeg','assets/anime_poster/asset\ 109.jpeg', 'assets/anime_poster/asset\ 55.jpeg','assets/anime_poster/asset\ 69.jpeg','assets/anime_poster/asset\ 89.jpeg','assets/anime_poster/asset\ 66.jpeg', 'assets/anime_poster/asset\ 62.jpeg','assets/anime_poster/asset\ 75.jpeg', 'assets/anime_poster/asset\ 82.jpeg','assets/anime_poster/asset\ 98.jpeg', 'assets/anime_poster/asset\ 110.jpeg','assets/anime_poster/asset\ 56.jpeg', 'assets/anime_poster/asset\ 70.jpeg','assets/anime_poster/asset\ 90.jpeg', 'assets/anime_poster/asset\ 104.jpeg','assets/anime_poster/asset\ 63.jpeg', 'assets/anime_poster/asset\ 76.jpeg','assets/anime_poster/asset\ 83.jpeg', 'assets/anime_poster/asset\ 97.jpeg','assets/anime_poster/asset\ 111.jpeg', 'assets/anime_poster/asset\ 57.jpeg','assets/anime_poster/asset\ 71.jpeg', 'assets/anime_poster/asset\ 91.jpeg','assets/anime_poster/asset\ 105.jpeg','assets/anime_poster/asset\ 64.jpeg','assets/anime_poster/asset\ 77.jpeg', 'assets/anime_poster/asset\ 84.jpeg','assets/anime_poster/asset\ 96.jpeg', 'assets/anime_poster/asset\ 112.jpeg','assets/anime_poster/asset\ 58.jpeg', 'assets/anime_poster/asset\ 71.jpeg','assets/anime_poster/asset\ 92.jpeg', 'assets/anime_poster/asset\ 106.jpeg','assets/anime_poster/asset\ 65.jpeg', 'assets/anime_poster/asset\ 78.jpeg','assets/anime_poster/asset\ 85.jpeg', 'assets/anime_poster/asset\ 95.jpeg','assets/anime_poster/asset\ 113.jpeg', 'assets/anime_poster/asset\ 59.jpeg','assets/anime_poster/asset\ 72.jpeg', 'assets/anime_poster/asset\ 93.jpeg','assets/anime_poster/asset\ 107.jpeg', 'assets/anime_poster/asset\ 116.jpeg','assets/anime_poster/asset\ 79.jpeg','assets/anime_poster/asset\ 86.jpeg','assets/anime_poster/asset\ 94.jpeg', 'assets/anime_poster/asset\ 114.jpeg',
+        'assets/anime_poster/asset\ 115.jpeg','assets/anime_poster/asset\ 103.jpeg'
+    ]
+const anime_lover = [
+    'assets/anime_lover/asset\ 56.jpeg' , 'assets/anime_lover/asset\ 57.jpeg','assets/anime_lover/asset\ 58.jpeg','assets/anime_lover/asset\ 59.jpeg','assets/anime_lover/asset\ 60.jpeg','assets/anime_lover/asset\ 61.jpeg','assets/anime_lover/asset\ 62.jpeg','assets/anime_lover/asset\ 63.jpeg','assets/anime_lover/asset\ 64.jpeg','assets/anime_lover/asset\ 65.jpeg','assets/anime_lover/asset\ 66.jpeg','assets/anime_lover/asset\ 67.jpeg','assets/anime_lover/asset\ 68.jpeg','assets/anime_lover/asset\ 69.jpeg','assets/anime_lover/asset\ 81.jpeg','assets/anime_lover/asset\ 82.jpeg','assets/anime_lover/asset\ 83.jpeg','assets/anime_lover/asset\ 84.jpeg','assets/anime_lover/asset\ 85.jpeg'
+]
+   console.log(anime_lover.length)
+
+const key_chains = [
+    'assets/keychains/asset\ 70.jpeg',  'assets/keychains/asset\ 71.jpeg',  'assets/keychains/asset\ 72.jpeg',  'assets/keychains/asset\ 73.jpeg',  'assets/keychains/asset\ 74.jpeg', 'assets/keychains/asset\ 75.jpeg',  'assets/keychains/asset\ 76.jpeg',  'assets/keychains/asset\ 77.jpeg',  'assets/keychains/asset\ 78.jpeg',  'assets/keychains/asset\ 79.jpeg', 'assets/keychains/asset\ 80.jpeg'
+] 
+
+
+const myhero = [
+    'assets/my_Hero_regular/asset\ 153.jpeg' , 'assets/my_Hero_regular/asset\ 154.jpeg','assets/my_Hero_regular/asset\ 159.jpeg','assets/my_Hero_regular/asset\ 156jpeg','assets/my_Hero_regular/asset\ 161.jpeg','assets/my_Hero_regular/asset\ 158.jpeg','assets/my_Hero_regular/asset\ 155.jpeg','assets/my_Hero_regular/asset\ 160.jpeg','assets/my_Hero_regular/asset\ 157.jpeg','assets/my_Hero_regular/asset\ 162.jpeg','assets/my_Hero_regular/asset\ 163.jpeg','assets/my_Hero_regular/asset\ 165.jpeg','assets/my_Hero_regular/asset\ 164.jpeg','assets/my_Hero_regular/asset\ 166.jpeg','assets/my_Hero_regular/asset\ 167.jpeg',
+]
+
+const oversized_titans = [
+    'assets/oversized_titans/asset\ 139.jpeg', 'assets/oversized_titans/asset\ 140.jpeg','assets/oversized_titans/asset\ 145.jpeg','assets/oversized_titans/asset\ 141.jpeg','assets/oversized_titans/asset\ 146.jpeg','assets/oversized_titans/asset\ 143.jpeg','assets/oversized_titans/asset\ 142.jpeg','assets/oversized_titans/asset\ 147.jpeg','assets/oversized_titans/asset\ 144.jpeg', 'assets/oversized_titans/asset\ 148.jpeg','assets/oversized_titans/asset\ 149.jpeg','assets/oversized_titans/asset\ 150.jpeg','assets/oversized_titans/asset\ 151.jpeg', 'assets/oversized_titans/asset\ 152.jpeg'
+]
+
+const regular_ragn = [
+    'assets/regular_ragnnarok/asset\ 186.jpeg','assets/regular_ragnnarok/asset\ 187.jpeg','assets/regular_ragnnarok/asset\ 188.jpeg','assets/regular_ragnnarok/asset\ 189.jpeg','assets/regular_ragnnarok/asset\ 192.jpeg','assets/regular_ragnnarok/asset\ 191.jpeg','assets/regular_ragnnarok/asset\ 190.jpeg',
+]
+
+const sanatani = [
+    'assets/sanatni/asset\ 193.jpeg', 'assets/sanatni/asset\ 197.jpeg','assets/sanatni/asset\ 194.jpeg', 'assets/sanatni/asset\ 198.jpeg',
+    'assets/sanatni/asset\ 195.jpeg', 'assets/sanatni/asset\ 199.jpeg','assets/sanatni/asset\ 196.jpeg'
+
+]
+
+const superHero = [
+    'assets/super_hero/asset\ 49.jpeg', 'assets/super_hero/asset\ 53.jpeg',
+    'assets/super_hero/asset\ 50.jpeg', 'assets/super_hero/asset\ 54.jpeg',
+    'assets/super_hero/asset\ 51.jpeg', 'assets/super_hero/asset\ 55.jpeg',
+    'assets/super_hero/asset\ 52.jpeg'
+]
+
+const tokyoRegular = [
+'assets/tokyo_regular/asset\ 168.jpeg','assets/tokyo_regular/asset\ 177.jpeg',
+'assets/tokyo_regular/asset\ 169.jpeg','assets/tokyo_regular/asset\ 178.jpeg',
+'assets/tokyo_regular/asset\ 170.jpeg','assets/tokyo_regular/asset\ 179.jpeg',
+'assets/tokyo_regular/asset\ 171.jpeg','assets/tokyo_regular/asset\ 180.jpeg',
+'assets/tokyo_regular/asset\ 172.jpeg','assets/tokyo_regular/asset\ 181.jpeg',
+'assets/tokyo_regular/asset\ 173.jpeg','assets/tokyo_regular/asset\ 182.jpeg',
+'assets/tokyo_regular/asset\ 174.jpeg','assets/tokyo_regular/asset\ 183.jpeg',
+'assets/tokyo_regular/asset\ 175.jpeg','assets/tokyo_regular/asset\ 184.jpeg',
+'assets/tokyo_regular/asset\ 176.jpeg','assets/tokyo_regular/asset\ 185.jpeg'
+]
+
+const videoGame = [
+    'assets/video_game/asset\ 118.jpeg','assets/video_game/asset\ 125.jpeg',
+    'assets/video_game/asset\ 119.jpeg','assets/video_game/asset\ 126.jpeg',
+    'assets/video_game/asset\ 120.jpeg','assets/video_game/asset\ 127.jpeg',
+    'assets/video_game/asset\ 121.jpeg','assets/video_game/asset\ 128.jpeg',
+    'assets/video_game/asset\ 122.jpeg','assets/video_game/asset\ 129.jpeg',
+    'assets/video_game/asset\ 123.jpeg','assets/video_game/asset\ 130.jpeg',
+    'assets/video_game/asset\ 124.jpeg','assets/video_game/asset\ 131.jpeg',
+    'assets/video_game/asset\ 132.jpeg','assets/video_game/asset\ 133.jpeg',
+]
+
+   let container = document.getElementById("accordion-bodyd")
+   let anime_3d_metal = document.getElementById("Anime_poster_metal")
+   let anime3d = document.getElementById("Anime-3d-pster")
+   let Anime_love = document.getElementById("Anime_love");
+   let keychains = document.getElementById("key-rings")
+   let MyHero = document.getElementById("MyHero")
+   let oversized_tita = document.getElementById("Oversized-titan")
+   let regano_regular = document.getElementById("regnarok_regular")
+   let sanaTni = document.getElementById("Sanatani-tr")  
+   let tokyo_revenssd = document.getElementById("tokyo_revengers") 
+   let video_games_ = document.getElementById("Video_Game") 
+
+
+
+
+
+
+
+   function generateCard(arr) {
+    return `
+        <div class="card" style="width: 18rem;" >
+            <a href="#productdetail">
+            <img src="${arr}" class="card-img-top product-Img" alt="..." width="50px" class="product-Img" style = "cursor:pointer;" onclick="window.location.href='sproduct.html';" ></a>
+       
+    
+
+
+    <div class="card-body">
+      <h5 class="card-title">AoT: The  Finale Os</h5>
+      
+      <p>₹399 <del>₹699</del></p>
+      <p> <span> (43% Off) </span> </p>
+      <a href="${arr}" class="btn btn-primary">Add To Cart</a>
+    </div>
+  </div>
+
+
+    `;
+}
+
+let card = document.getElementsByClassName("card");
+// card.onclick = "window.location.herf = 'sproduct.html';"
+
+function recommonded(conta , array){
+    
+    
+    // Create an empty array to store the card HTML
+    const cards = [];
+    
+    // Generate card HTML for each image URL
+    for (const url of array) {
+        cards.push(generateCard(url));
+    }
+    
+    // Join the card HTML strings into a single string
+    const allCardsHtml = cards.join("");
+    
+    // Append the cards to the body
+    conta.innerHTML += allCardsHtml;
+}
+recommonded(container ,arr)
+recommonded(anime3d , anime)
+recommonded(anime_3d_metal , animemetalcot)
+recommonded(Anime_love , anime_lover)
+recommonded(keychains, key_chains)
+recommonded(MyHero, superHero)
+recommonded(oversized_tita, oversized_titans)
+recommonded(regano_regular,regular_ragn)
+recommonded(sanaTni,sanatani)
+recommonded(tokyo_revenssd, tokyoRegular)
+
+let chatbox = document.getElementById("chat-box")
+let leftimg = document.getElementById("left_img")
+
+let cross = document.getElementById("cross")
+ 
+leftimg.addEventListener('click', ()=>{
+
+
+    // if(x == "none"){
+ chatbox.style.display = "block"
+    // }
+    // if(x == "block"){
+    //     x = "none"
+    // }
+});
+cross.addEventListener('click', ()=>{
+
+
+    // if(x == "none"){
+ chatbox.style.display = "none"
+    // }
+    // if(x == "block"){
+    //     x = "none"
+    // }
+});
+
+
+
+// leftimg.addEventListener('click', ()=>{
+
+
+//     // if(x == "none"){
+//         // chatbox.style.display = "block"
+//     // }
+//     if(chatbox.style.display == "block"){
+//         chatbox.style.display = "none"
+//     }
+// });
+
+
+let t = document.getElementById("no_of_recommended")
+
+
+t.innerHTML =`${arr.length + " " + "items"}`
+
+
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
+const chatMessages = document.querySelector('.chat-messages');
+
+
+
+sendButton.addEventListener('click', () => {
+  const message = messageInput.value;
+  if (message !== '') {
+    const messageHTML = `
+      <div class="message">
+        <p>${message}</p>
+        <span>${new Date().toLocaleTimeString()}</span>
+      </div>
+    `;
+    chatMessages.innerHTML += messageHTML;
+    messageInput.value = '';
+  }
+});
